5837ceccf990aaa65ef9f700e09b8537808dc6e3
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2022-01-12 15:36:47 +0800

    Music player with javascript

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jheytim",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jheytim",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jheytim",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Jheytim",
+  },
+];
 
 // Check if Playing
 let isPlaying = false;
@@ -23,5 +50,38 @@ const pauseSong = () => {
 };
 
 // Play or pause event listener
-
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+const loadSong = (song) => {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+};
+
+// Current songs
+let songIndex = 0;
+
+// Next song
+const nextSong = () => {
+  songIndex++;
+  if (songIndex > songs.length - 1) songIndex = 0;
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+// Previous Song
+const prevSong = () => {
+  songIndex--;
+  if (songIndex < 0) songIndex = songs.length - 1;
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+// On Load - Select first songs
+loadSong(songs[songIndex]);
+
+// Event listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
